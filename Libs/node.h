ec7148655cd77dfc78cdb596347ba15f652f0526
tyd93 <sebak93@gmail.com>
2015-09-08 09:12:08 -0300

    se añadieron funciones y se aclaro la idea

--- Libs/node.h
+++ Libs/node.h
@@ -1,4 +1,4 @@
-#ifndef STDIO_H
+﻿#ifndef STDIO_H
 #include <stdio.h>
 #define STDIO_H
 #endif
@@ -10,18 +10,25 @@
 
 #define largo 20
 
+/*
+Modificare los nodos de dirección, ya que este grafo tendra que tener
+más de dos direcciones.
+Al principio lo trate como una lista enlazada.
+08:35 08/09/2015
+*/
+
 typedef struct tNodo{
-	struct nodo *ant;
-	struct nodo *sig;
+	//struct nodo 
 	char nombre[largo]; //Largo maximo de nuestros participantes de la red social.
 	int color; // Se manejara el color como una variable entera. 0 es blanco, 1 es gris, 2 es negro.
-	int nUsuario; // Basicamente lo usare para enumerar los usuarios. Vere la utilidad de esta función más adelante.
 }nodo;
 
 typedef struct tLista{
-	struct nodo *raiz;
-	int total;
-}red;
+	int V;
+	nodo **
+}grafo;
+
+
 
 /*
 	Esbozo primario de lo que sería esta red social, la cual
@@ -29,3 +36,9 @@ typedef struct tLista{
 	para aplicar los algoritmos de BFS y DFS.
 	07-09-2015 21:28
 */
+
+/*Usare lista enlazada
+ * de adyacencia
+ *08:59 08/09/2015
+ * /
+ 
